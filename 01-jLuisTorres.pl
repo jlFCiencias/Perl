8325989bcac0c710434b5655971dcff53c0091fd
jluis <J. Luis>
2017-02-02 21:38:20 -0600

    Subiendo tarea 1.

---
+++ 01-jLuisTorres.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+ 
+print "Definicion de pragma:\n\nUn pragma es un modulo de Perl que permite afectar de alguna forma el proceso de compilacion o de ejecucion de un programa, aplicando ciertas directivas o reglas contenidas en el mismo.\nUn pragma se incluye en un programa en Perl por medio del comando \"use\".\nUn ejemplo de pragma es \"strict\", el cual genera mensajes de error, por ejemplo cuando se detectan variables no inicializadas o referencias inexistentes.\n";
+
+__END__
+
+=pod
+ 
+=head1 Jose Luis Torres Rodriguez - Tarea 1
+
+=head2 DESCRIPCION
+
+Ejemplo del uso de B<pod> para la creacion de documentacion de programas en Perl. 
+
+Muestra en la terminal una descripcion de B<pragma>, de acuerdo a su uso en Perl.
+
+=head2 PARAMETROS
+
+El programa no recibe parametros. 
+
+=head2 VALORES DEVUELTOS
+
+No devuelve valores, solamente muestra la informacion mencionada en la descripcion.
+
+=head2 EJECUCION
+
+Para ejecutarlo es suficiente con verificar que cuenta con los permisos de ejecucion correspondientes y hacer uso de 
+
+B<C<./01-jLuisTorres.pl>> 
+
+o bien 
+
+B<C<perl 01-jLuisTorres.pl>>
+ 
+=cut
